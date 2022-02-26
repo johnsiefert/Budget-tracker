bcd951df983b2cac817bd99efdc2b74408beba44
johnsiefert <johnsief12@gmail.com>
2022-02-26 12:55:51 -0500

    first commit with service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,75 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+    "/",
+  "/index.html",
+  "/manifest.json",
+  "/css/styles.css",
+  "/js/index.js",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+];
+
+// Responds with cached resources
+self.addEventListener('fetch', function (evt) {
+  console.log('fetch request : ' + evt.request.url)
+  evt.respondWith(
+    caches
+    .open(CACHE_NAME)
+    .then(cache => {
+      return fetch(evt.request)
+        .then(response => {
+            if (response.status === 200) {
+            cache.put(evt.request.url, response.clone());
+          }
+
+          return response;
+        })
+        .catch(err => {
+
+          return cache.match(evt.request);
+        });
+    })
+    .catch(err => console.log(err))
+  );
+
+  return;
+})
+
+
+// install service worker
+self.addEventListener('install', function (e) {
+   e.waitUntil(
+    // find specific cache by name, then add every file in the files to cache array to cache
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+        caches.keys().then(function (keyList) {
+          let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
+});
