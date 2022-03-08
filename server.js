5a26418f78d700a73a34074d8bd702228f269a29
johnsiefert <johnsief12@gmail.com>
2022-03-07 19:00:24 -0500

    deploying

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budgets', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
