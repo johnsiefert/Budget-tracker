28eb07db82b8b408511193c5ff93885ea293ceda
johnsiefert <johnsief12@gmail.com>
2022-03-07 19:20:34 -0500

    fixed

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgets";
 
 const app = express();
 
